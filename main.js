756e25caf9bb4a3f2b37d12b05d1086c45ab24b0
Rupam Kairi <rupamkairi@hotmail.com>
2019-01-04 23:48:38 +0530

    dark-theme, paths instead of points and lil more

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-canvas.style.backgroundColor = "white";
+
+// DARK-MODE BABY HELL YEAH !   LOOK AT ctx.strokeStyle and ctx.fillStyle THROUGHOUT THE CODE.
+canvas.style.backgroundColor = "black";
 
 function resize(w, h) {
     canvas.width = w;
     canvas.height = h;
 }
 
-let nw = 1;
+let nw = 7;
 
 window.onload = () => {
     resize(window.innerWidth, window.innerHeight);
+
+    // NO NEED TO ASK FOR NW ------------------------ (FOR ME)
     nw = prompt("Enter the number of terms: ", 1);
     if (nw < 1) {
         nw = 1;
     }
+    // ----------------------------------------------
 }
 
 window.onresize = () => {
@@ -30,7 +35,7 @@ let fpsInterval, startTime, now, then, elapsed;
  * @param {*} fps Frames per second.
  */
 function startAnimation(fps) {
-    fpsInterval = 1000/fps;
+    fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
     animate();
@@ -44,12 +49,11 @@ function animate() {
     now = Date.now();
     elapsed = now - then;
     if (elapsed > fpsInterval) {
-        then = now - (elapsed%fpsInterval)
+        then = now - (elapsed % fpsInterval)
 
         // Draw and update here
         draw();
     }
-
 }
 
 /** ------------------------------------------------------------------------------------- */
@@ -61,10 +65,10 @@ function animate() {
  * @param {int} r Radius of the circle
  * @param {int} lineWidth By default 0 and fills the circle
  */
-function circle(x, y, r, lineWidth=0) {
+function circle(x, y, r, lineWidth = 0) {
     ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.arc(x, y, r, 0, 2*Math.PI);
+    ctx.strokeStyle = "white";
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
     if (lineWidth == undefined || lineWidth == 0) {
         ctx.fill();
     } else {
@@ -72,7 +76,6 @@ function circle(x, y, r, lineWidth=0) {
         ctx.stroke();
     }
     ctx.closePath();
-    
 }
 
 /**
@@ -88,7 +91,7 @@ function circle(x, y, r, lineWidth=0) {
  */
 function line(x1, y1, x2, y2, lineWidth) {
     ctx.beginPath();
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = "white";
     if (lineWidth == undefined) {
         ctx.lineWidth = lineWidth;
     }
@@ -105,11 +108,22 @@ function line(x1, y1, x2, y2, lineWidth) {
  * @param {*} radius The radius of the point... if undefined, its
  *                   default value i.e, 2 will be used.
  */
+// THIS POINT FUNCTION LOOKS BIT SQUARE SO I CHANGED FUNCTIONALITY ----------------
 function point(x, y, radius) {
     if (radius == undefined || radius == NaN) {
-        ctx.fillRect(x, y, 1, 1);
+        // ctx.fillRect(x, y, 1, 1);
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.moveTo(x - 2, y - 2);
+        ctx.arc(x, y, radius, 0, 2 * Math.PI)
+        ctx.fill()
     } else {
-        ctx.fillRect(x, y, radius, radius);
+        // ctx.fillRect(x, y, radius, radius);
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.moveTo(x - radius, y - radius);
+        ctx.arc(x, y, radius, 0, 2 * Math.PI)
+        ctx.fill()
     }
 }
 
@@ -119,27 +133,28 @@ function point(x, y, radius) {
 let r = 60;
 let theta = 0;
 let points = [];
+let animation_speed = 3;        // INCREASING MORE THAN 5 MAY CAUSE WEIRDNESS IN GRAPH.
 
 /**
  * Do the drawing inside the following function.
  */
 function draw() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     generic(nw);
 
 }
 
 function generic(n) {
-    let x = window.innerWidth/4;
-    let y = window.innerHeight/2;
+    let x = window.innerWidth / 4;
+    let y = window.innerHeight / 2;
 
     point(x, y, 3);
 
-    for (i=0; i<n; i++) {
+    for (i = 0; i < n; i++) {
 
         let newradius = r * (4 / (((2 * i) + 1) * Math.PI));
-        
+
         let prevx = x;
         let prevy = y;
 
@@ -158,26 +173,38 @@ function generic(n) {
 
         line(prevx, prevy, x, y);
 
-        point(x, y, 3);
+        point(x, y, 2);
     }
 
     points.unshift(y);
 
-    theta += 0.01;
-    
-    // Draw the wave.
-    let ax = window.innerWidth/2;
-    for (i=0; i<points.length; i++) {
-        point(ax, points[i]);
-        ax+=0.2;
-    }
+    // THIS INCREMENT CHANGES THE ANIMATION SPEED ------ I'MEAN OF COURSE, WHY NOT !
+    theta += 0.01 * animation_speed;
+    // SO ANIMATION STUFF IS FOR YOU TO MAKE........................................
+    // -----------------------------------------------------------------------------
 
-    // If length of points exceeds 1300, pop the extra points.
-    if (points.length > 1300) {
+    // Draw the wave.
+    let ax = window.innerWidth / 2;
+
+    // DRAWING OF POINTS CHANGED BY DRAWING OF PATH SO THAT HOW FAR TWO POINTS CAN BE PLACED ALWAYS HAVE A LINE BETWEEN THEM.
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.moveTo(ax, points[0]);
+    for (i = 0; i < points.length; i++) {
+        ctx.lineTo(ax, points[i]);
+        ax += 0.3 * animation_speed;
+    }
+    ctx.stroke();
+
+    // If length of points exceeds 1300, pop the extra points. (NOPE! IT'S COMPLICATED NOW)=================================
+    if (points.length > (1 / animation_speed) * 1500) {
         points.pop();
     }
-    
-    line(x, y, window.innerWidth/2, points[0]);
+
+    line(x, y, window.innerWidth / 2, points[0]);
 }
 
-startAnimation(60);
+// INCREASE THE ANIMATION SPEED -----------
+// startAnimation(60);
+startAnimation(180);            // FUUUUCK THIS DID NOT WORK.
+// ---------------------------------------- THIS NEED TO BE WORKED OUT ! WITH theta ! THAT'S A WAY. MAYBE.
